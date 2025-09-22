8e141597aa3e05f5212010d9243093242866b206
lesley_w <lrwesley92@gmail.com>
2025-09-21 20:07:27 -0500

    Added basic jsx for CurrentForecast component

--- src/components/CurrentForecast.jsx
+++ src/components/CurrentForecast.jsx
@@ -1,7 +1,37 @@
 export default function CurrentForecast() {
   return (
     <section className="current-forecast-section">
-      <h2>This is the Current Forecast Section</h2>
+    //Code for Main Widget
+      <div className="main-widget">
+          <div className="current-info">
+              <h2 className="location">Berlin, Germany</h2>
+              <time datetime="2025-09-05">Tuesday, Aug 5, 2025</time>
+          </div>
+          <div className="current-temp-section">
+              <img src="./src/assets/icon-sunny.webp" alt="Sunny" />
+              <span className="current-temp-section">20°</span>
+          </div>
+      </div>
+
+      //Code for Current Details section
+      <div className="current-details">
+          <div className="current-detail-card">
+            <h3>Feels Like</h3>
+            <span className="current-data">18°</span>
+          </div>
+          <div className="current-detail-card">
+            <h3>Humidity</h3>
+            <span className="current-data">46%</span>
+          </div>
+          <div className="current-detail-card">
+            <h3>Wind</h3>
+            <span className="current-data">14 km/h</span>
+          </div>
+          <div className="current-detail-card">
+            <h3>Precipitation</h3>
+            <span className="current-data">0 mm</span>
+          </div>
+      </div>
     </section>
   )
 }
