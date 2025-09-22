89e7201d403fbf2d8b5f9d1990fb98252129abc7
lesley_w <lrwesley92@gmail.com>
2025-09-22 18:55:10 -0500

    Added mobile styling to Current Details section of MainContent component

--- src/components/CurrentForecast.jsx
+++ src/components/CurrentForecast.jsx
@@ -1,7 +1,7 @@
 export default function CurrentForecast() {
   return (
     <section className="current-forecast-section">
-    //Code for Main Widget
+
       <div className="main-widget">
           <div className="current-info">
               <span className="location preset-4">Berlin, Germany</span>
@@ -13,23 +13,23 @@ export default function CurrentForecast() {
           </div>
       </div>
 
-      //Code for Current Details section
+
       <div className="current-details">
           <div className="current-detail-card">
-            <h2>Feels Like</h2>
-            <span className="current-data">18°</span>
+            <h2 className="preset-6">Feels Like</h2>
+            <span className="preset-3">18°</span>
           </div>
           <div className="current-detail-card">
-            <h2>Humidity</h2>
-            <span className="current-data">46%</span>
+            <h2 className="preset-6">Humidity</h2>
+            <span className="preset-3">46%</span>
           </div>
           <div className="current-detail-card">
-            <h2>Wind</h2>
-            <span className="current-data">14 km/h</span>
+            <h2 className="preset-6">Wind</h2>
+            <span className="preset-3">14 km/h</span>
           </div>
           <div className="current-detail-card">
-            <h2>Precipitation</h2>
-            <span className="current-data">0 mm</span>
+            <h2 className="preset-6">Precipitation</h2>
+            <span className="preset-3">0 mm</span>
           </div>
       </div>
     </section>
