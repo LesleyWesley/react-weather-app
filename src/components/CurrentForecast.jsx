5524d0dc5625194f6d9c563826f953688c9f0029
lesley_w <lrwesley92@gmail.com>
2025-09-22 16:02:54 -0500

    Added colors and font to css file

--- src/components/CurrentForecast.jsx
+++ src/components/CurrentForecast.jsx
@@ -4,7 +4,7 @@ export default function CurrentForecast() {
     //Code for Main Widget
       <div className="main-widget">
           <div className="current-info">
-              <h2 className="location">Berlin, Germany</h2>
+              <span className="location">Berlin, Germany</span>
               <time datetime="2025-09-05">Tuesday, Aug 5, 2025</time>
           </div>
           <div className="current-temp-section">
@@ -16,19 +16,19 @@ export default function CurrentForecast() {
       //Code for Current Details section
       <div className="current-details">
           <div className="current-detail-card">
-            <h3>Feels Like</h3>
+            <h2>Feels Like</h2>
             <span className="current-data">18°</span>
           </div>
           <div className="current-detail-card">
-            <h3>Humidity</h3>
+            <h2>Humidity</h2>
             <span className="current-data">46%</span>
           </div>
           <div className="current-detail-card">
-            <h3>Wind</h3>
+            <h2>Wind</h2>
             <span className="current-data">14 km/h</span>
           </div>
           <div className="current-detail-card">
-            <h3>Precipitation</h3>
+            <h2>Precipitation</h2>
             <span className="current-data">0 mm</span>
           </div>
       </div>
