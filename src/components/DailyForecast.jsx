d9df64a61b8b15338f6c10ceca15ac32a52169b8
lesley_w <lrwesley92@gmail.com>
2025-09-23 11:44:35 -0500

    Added basic jsx for daily forecast section

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -1,7 +1,17 @@
 export default function DailyForecast() {
   return (
     <section className="daily-forecast-section">
-      <h2>This is where the Daily Forecast Section will go.</h2>
+      <h3 className="preset-5">Daily forecast</h3>
+      <div className="daily-card-container">
+          <article className="day-card">
+              <h4 className="preset-6">Tue</h4>
+              <img src="./src/assets/icon-rain.webp" alt="Rainy" className="daily-icon"/>
+              <div className="day-temp-container">
+                <span className="daily-high preset-7">20°</span>
+                <span className="daily-low preset-7">14°</span>
+              </div>
+          </article>
+      </div>
     </section>
   )
 }
