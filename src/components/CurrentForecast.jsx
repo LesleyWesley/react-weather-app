a1444b52af52c55854dfa21bb17cb5064df10dc1
lesley_w <lrwesley92@gmail.com>
2025-09-22 21:16:09 -0500

    Added tablet styling for MainContent component

--- src/components/CurrentForecast.jsx
+++ src/components/CurrentForecast.jsx
@@ -3,13 +3,15 @@ export default function CurrentForecast() {
     <section className="current-forecast-section">
 
       <div className="main-widget">
-          <div className="current-info">
-              <span className="location preset-4">Berlin, Germany</span>
-              <time datetime="2025-09-05" className="preset-6">Tuesday, Aug 5, 2025</time>
-          </div>
-          <div className="current-temp-section">
-              <img src="./src/assets/icon-sunny.webp" alt="Sunny" className="weather-icon"/>
-              <span className="current-temp-section preset-1">20°</span>
+          <div className="main-widget-container">
+            <div className="current-info">
+                <span className="location preset-4">Berlin, Germany</span>
+                <time datetime="2025-09-05" className="preset-6">Tuesday, Aug 5, 2025</time>
+            </div>
+            <div className="current-temp-section">
+                <img src="./src/assets/icon-sunny.webp" alt="Sunny" className="weather-icon"/>
+                <span className="current-temp-section preset-1">20°</span>
+            </div>
           </div>
       </div>
 
