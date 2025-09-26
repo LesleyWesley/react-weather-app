2ed589476fc050d6627ebbdfb9e3455eb432daea
lesley_w <lrwesley92@gmail.com>
2025-09-25 20:47:53 -0500

    Added mobile styling for HourlyForecast component

--- src/components/HourlyCard.jsx
+++ src/components/HourlyCard.jsx
@@ -1,11 +1,13 @@
 export default function HourlyCard() {
   return (
     <article className="hourly-card">
-      <div className="hourly-card-left">
-          <img src="./src/assets/icon-overcast.webp" alt="overcast" />
-          <h4 className="preset-5">3 PM</h4>
+      <div className="hourly-content-container">
+        <div className="hourly-card-left">
+            <img src="./src/assets/icon-overcast.webp" alt="overcast" className="hourly-icon"/>
+            <h4 className="preset-5">3 PM</h4>
+        </div>
+        <span className="preset-7">20°</span>
       </div>
-      <span className="preset-7">20°</span>
     </article>
   )
 }
