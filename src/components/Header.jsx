1c2cc18f9b3ec48356168a0897f624219ddf1a6b
lesley_w <lrwesley92@gmail.com>
2025-09-27 17:09:18 -0500

    Added basic header jsx and mobile styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header() {
   return (
     <header>
-      <p>This is the Header Section</p>
+      <div className="header-left">
+        <img src="./src/assets/logo.svg" alt="Weather Now Logo" />
+      </div>
+      <div className="header-right">
+        <p>Units Dropdown</p>
+      </div>
     </header>
   )
 }
