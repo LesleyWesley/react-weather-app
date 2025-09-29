e63cf55a2d6b8e2f793583d1de3c82580de9a6b8
lesley_w <lrwesley92@gmail.com>
2025-09-29 16:16:21 -0500

    Added jsx for searchbar section and added mobile styling for same

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,7 +1,13 @@
+
 export default function SearchSection() {
+
   return (
-    <section>
-      <h1>This is where the Search Section will go.</h1>
+    <section className="search-section">
+      <h1 className="preset-2">How's the sky looking today?</h1>
+      <form className="search-form">
+        <input type="text" aria-label="Search for location" placeholder="Search for a place..." className="search-input preset-5"/>
+        <button type="submit" className="search-button preset-5">Search</button>
+      </form>
     </section>
   )
 }
