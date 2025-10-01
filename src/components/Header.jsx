9cff731896984cd9a80d51cdb30c8b55838e9504
lesley_w <lrwesley92@gmail.com>
2025-10-01 14:12:17 -0500

    Added toggle ability using state to modals

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
-import UnitDropdownModal from "./UnitDropdownModal"
+import UnitDropdownModal from "./UnitDropdownMenu"
+import React from "react"
 
 export default function Header() {
+
+  const [unitModalOpen, setUnitModalOpen] = React.useState(false);
+
+  function handleClick() {
+    setUnitModalOpen(prevUnitModalOpen => !prevUnitModalOpen)
+  }
+
   return (
     <header className="site-header">
       <div className="header-left">
         <img src="./src/assets/logo.svg" alt="Weather Now Logo" />
       </div>
       <div className="header-right">
-        <button buttontype="button" className="units-dropdown-button preset-7">
+        <button buttontype="button" className="units-dropdown-button preset-7" onClick={handleClick}>
           <img src="./src/assets/icon-units.svg" alt="Units Icon" />
           Units
           <img src="./src/assets/icon-dropdown.svg" alt="dropdown icon" />
         </button>
-        <UnitDropdownModal />
+        {unitModalOpen && <UnitDropdownModal />}
       </div>
     </header>
   )
--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,16 +1,24 @@
 import HourlyCard from "./HourlyCard"
 import DayDropdownModal from "./DayDropdownModal"
+import React from "react"
 
 export default function HourlyForecast() {
+
+  const [open, setOpen] = React.useState(false)
+
+  function handleClick() {
+    setOpen(prevOpen => !prevOpen)
+  }
+
   return (
     <section className="hourly-forecast-section">
         <header>
           <h3 className="preset-5">Hourly forecast</h3>
-          <button type="button" className="day-dropdown">
+          <button type="button" className="day-dropdown" onClick={handleClick}>
             Tuesday
             <img src="./src/assets/icon-dropdown.svg" alt="" />
           </button>
-          <DayDropdownModal />
+          {open && <DayDropdownModal />}
         </header>
         <div className="hourly-card-container">
             <HourlyCard
