8c58abd3568e72c3bd26716988e1325e5e16e51b
lesley_w <lrwesley92@gmail.com>
2025-10-05 18:44:39 -0500

    Added base jsx and styling for DayDropdownModal

--- src/components/DayDropdownModal.jsx
+++ src/components/DayDropdownModal.jsx
@@ -1,8 +1,37 @@
 export default function DayDropdownModal() {
+
+
   return (
-    <div className="day-dropdown-modal">
-      <p>This is where the day dropdown will go</p>
-    </div>
+    <fieldset className="day-dropdown-modal">
+      <label>Monday
+        <input type="radio" name="dayOfWeek" value="monday" />
+      </label>
+
+      <label>Tuesday
+        <input type="radio" name="dayOfWeek" value="tuesday" defaultChecked={true}/>
+      </label>
+
+      <label>Wednesday
+        <input type="radio" name="dayOfWeek" value="wednesday" />
+      </label>
+
+      <label>Thursday
+        <input type="radio" name="dayOfWeek" value="thursday" />
+      </label>
+
+      <label>Friday
+        <input type="radio" name="dayOfWeek" value="friday" />
+      </label>
+
+      <label>Saturday
+        <input type="radio" name="dayOfWeek" value="saturday" />
+      </label>
+
+      <label>Sunday
+        <input type="radio" name="dayOfWeek" value="sunday" />
+      </label>
+
+    </fieldset>
   )
 }
 
